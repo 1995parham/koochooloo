94daa643135e72a45d811f2da095d789dd25d582
Parham Alvani <parham.alvani@gmail.com>
2020-02-11 13:26:19 +0330

    feat: Add Validator into Request

--- request/url.go
+++ request/url.go
@@ -1,10 +1,22 @@
 package request
 
-import "time"
+import (
+	"time"
 
-// URLReq represents short URL creation request
-type URLReq struct {
-	URL    string     `json:"url" validate:"required"`
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
+// URL represents short URL creation request
+type URL struct {
+	URL    string     `json:"url"`
 	Name   string     `json:"name"`
 	Expire *time.Time `json:"expire"`
+}
+
+// Validate URL request
+func (r URL) Validate() error {
+	return validation.ValidateStruct(&r,
+		validation.Field(&r.URL, validation.Required, is.URL),
+	)
 }
