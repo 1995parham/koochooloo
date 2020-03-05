ae52fd23736d3ba66ea5cb9669390597f9b3622d
Parham Alvani <parham.alvani@gmail.com>
2020-03-05 14:49:16 +0000

    feat: Update All Packages

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,7 +25,9 @@ func main(cfg config.Config) {
 		Store: store.URL{DB: db},
 	}.Register(app.Group("/api"))
 
-	app.Listen(":8080")
+	if err := app.Listen(":8080"); err != nil {
+		panic(err)
+	}
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
