4fb3b10382fe543c5e47f206c08f8fd8552b5304
Parham Alvani <parham.alvani@gmail.com>
2020-04-16 08:51:42 +0430

    feat: Add Mocked Store

---
+++ store/mock.go
@@ -0,0 +1,53 @@
+package store
+
+import (
+	"context"
+	"time"
+
+	"github.com/1995parham/koochooloo/model"
+)
+
+type MockURL struct {
+	store map[string]model.URL
+}
+
+func NewMockURL() *MockURL {
+	return &MockURL{
+		store: make(map[string]model.URL),
+	}
+}
+
+func (m MockURL) Inc(ctx context.Context, key string) error {
+	return nil
+}
+
+func (m MockURL) Set(ctx context.Context, key string, url string, expire *time.Time) (string, error) {
+	if key == "" {
+		key = Key()
+	} else {
+		key = "$" + key
+	}
+
+	if _, ok := m.store[key]; ok {
+		return "", ErrDuplicateKey
+	}
+
+	m.store[key] = model.URL{
+		Key:        key,
+		URL:        url,
+		Count:      0,
+		ExpireTime: expire,
+	}
+
+	return key, nil
+}
+
+func (m MockURL) Get(ctx context.Context, key string) (string, error) {
+	url := m.store[key]
+
+	if url.ExpireTime == nil || url.ExpireTime.After(time.Now()) {
+		return url.URL, nil
+	}
+
+	return "", ErrKeyNotFound
+}
