b223108bd5f8b592dfc0d537c5b01b290564b0e5
Parham Alvani <parham.alvani@gmail.com>
2021-05-19 13:10:56 +0430

    feat: Move Configurations into Monitor Package

--- internal/metric/server.go
+++ internal/metric/server.go
@@ -4,35 +4,42 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/1995parham/koochooloo/internal/config"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 )
 
+// Configs of metric servers.
+type Config struct {
+	Address string `koanf:"address"`
+	Enabled bool   `koanf:"enabled"`
+}
+
 // Server contains information about metrics server.
 type Server struct {
-	Address string
-	Enabled bool
+	srv     *http.ServeMux
+	address string
 }
 
 // NewServer creates a new monitoring server.
-func NewServer(cfg config.Monitoring) Server {
+func NewServer(cfg Config) Server {
+	var srv *http.ServeMux
+
+	if cfg.Enabled {
+		srv = http.NewServeMux()
+		srv.Handle("/metrics", promhttp.Handler())
+	}
+
 	return Server{
-		Address: cfg.Address,
-		Enabled: cfg.Enabled,
+		address: cfg.Address,
+		srv:     srv,
 	}
 }
 
 // Start creates and run a metric server for prometheus in new go routine.
 func (s Server) Start(logger *zap.Logger) {
-	if s.Enabled {
-		srv := http.NewServeMux()
-		srv.Handle("/metrics", promhttp.Handler())
-
-		go func() {
-			if err := http.ListenAndServe(s.Address, srv); !errors.Is(err, http.ErrServerClosed) {
-				logger.Error("metric server initiation failed", zap.Error(err))
-			}
-		}()
-	}
+	go func() {
+		if err := http.ListenAndServe(s.address, s.srv); !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("metric server initiation failed", zap.Error(err))
+		}
+	}()
 }
